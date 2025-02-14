e0d574f5fbf3e767188eb922ba7c484db7519425
DragonWrangler1 <joshua.b.newport@gmail.com>
2025-02-14 08:57:58 -0600

    update #2

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 local show_actions = minetest.settings:get_bool("vlf_cozy_print_actions", false)
 local EYE_OFFSETS = {sit = {x = 0, y = -7, z = 2}, lay = {x = 0, y = -13, z = -5}, default = {x = 0, y = 0, z = 0}}
-local cozy = {positions = {}, mods = {"vlf_title"}}
+local cozy = {positions = {}, mods = {"mcl_title"}}
 
 local function send_action(player_name, action)
 	if show_actions then
@@ -12,7 +12,7 @@ end
 local function show_status(player, message)
 	for _, mod in ipairs(cozy.mods) do
 		if minetest.get_modpath(mod) then
-			return mod == "vlf_title" and vlf_title.set(player, "actionbar", {text = message or S("Move to stand up"), color = "white", stay = 60})
+			return mod == "mcl_title" and mcl_title.set(player, "actionbar", {text = message or S("Move to stand up"), color = "white", stay = 60})
 		end
 	end
 	minetest.log("warning", "[vlf_cozy] No mod found for actionbar display!")
@@ -22,13 +22,13 @@ local function reset_player(player, name)
 	player:set_eye_offset(EYE_OFFSETS.default, EYE_OFFSETS.default)
 	playerphysics.remove_physics_factor(player, "speed", "vlf_cozy:attached")
 	playerphysics.remove_physics_factor(player, "jump", "vlf_cozy:attached")
-	vlf_player.player_attached[name] = false
+	mcl_player.player_attached[name] = false
 	cozy.positions[name] = nil
 end
 
 local function handle_action(player, name, action)
-	if vlf_playerinfo[name].node_stand_below.name == "air" then return end
-	if vlf_player.player_attached[name] then reset_player(player, name) send_action(name, "stand")
+	if mcl_playerinfo[name].node_stand_below.name == "air" then return end
+	if mcl_player.player_attached[name] then reset_player(player, name) send_action(name, "stand")
 	else
 		for _, pos in pairs(cozy.positions) do
 			if vector.distance(player:get_pos(), pos) < 1 then return show_status(player, S("This spot is already occupied!")) end
@@ -36,7 +36,7 @@ local function handle_action(player, name, action)
 		player:set_eye_offset(EYE_OFFSETS[action], EYE_OFFSETS[action])
 		playerphysics.add_physics_factor(player, "speed", "vlf_cozy:attached", 0)
 		playerphysics.add_physics_factor(player, "jump", "vlf_cozy:attached", 0)
-		vlf_player.player_attached[name] = true vlf_player.player_set_animation(player, action, action == "sit" and 30 or 0)
+		mcl_player.player_attached[name] = true mcl_player.player_set_animation(player, action, action == "sit" and 30 or 0)
 		cozy.positions[name] = player:get_pos() send_action(name, action) show_status(player)
 	end
 end
@@ -45,7 +45,7 @@ minetest.register_globalstep(function()
 	for _, player in ipairs(minetest.get_connected_players()) do
 		local name = player:get_player_name()
 		local ctrl = player:get_player_control()
-		if vlf_player.player_attached[name] and not player:get_attach() and (ctrl.up or ctrl.down or ctrl.left or ctrl.right or ctrl.jump or ctrl.sneak) then
+		if mcl_player.player_attached[name] and not player:get_attach() and (ctrl.up or ctrl.down or ctrl.left or ctrl.right or ctrl.jump or ctrl.sneak) then
 			reset_player(player, name) send_action(name, "stand")
 		elseif minetest.get_node(vector.offset(player:get_pos(), 0, -1, 0)).name == "air" then reset_player(player, name) end
 	end
